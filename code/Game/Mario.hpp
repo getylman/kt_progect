940df140e3759e8e94a2b314874a6ea6048b0da3
Ткачик Андрей <68420139+AndreyTkachik@users.noreply.github.com>
2023-05-03 15:10:35 +0300

    Update Mario.hpp

--- code/Game/Mario.hpp
+++ code/Game/Mario.hpp
@@ -6,27 +6,13 @@ class Mario
 	bool dead;
 	bool flipped;
 	bool on_ground;
-
-	//Oh, this is a funny story!
-	//So I was working on level 1-2 when I discovered a simple bug.
-	//The bug was that when 2 Goombas stand SUPER close to each other and Mario jumps on them, one goomba dies and the other kills Mario.
-	//This was happening because the first Goomba was setting Mario's vertical speed below 0 after dying.
-	//Then the second Goomba checked the collision with Mario and saw that Mario wasn't squishing anymore and instead was going upwards.
-	//So he was killing Mario.
-	//And by adding this variable, I fixed the bug.
-	//Hehe.
 	float enemy_bounce_speed;
 	float horizontal_speed;
 	float vertical_speed;
 	float x;
 	float y;
-
-	//The longer we press the jump button, the higher Mario jumps. And we use this variable for that.
 	unsigned char jump_timer;
-	//I was thinking about adding Fire Mario.
-	//But I figured that it's gonna be too much work and I'm too lazy for that.
-	//Maybe in the next part?
-	unsigned char powerup_state;
+	unsigned char powerup_state; // It will deside is we can shoot and destroy bricks or not
 
 	unsigned short death_timer;
 	unsigned short growth_timer;
